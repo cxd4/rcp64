4eee1ea4ff249dd3e145f6a32dd378a1d2eb1033
unknown <rj150@hotmail.com>
2015-04-26 18:56:55 -0400

    Apparently, many compilers allowed me to totally forget the semicolon. MSVC 2005 (under VS2005 Express), however, throws an error.

--- my_types.h
+++ my_types.h
@@ -257,14 +257,14 @@ typedef s64                     i64;
  * material to deduce any of our 8-, 16-, 32-, or 64-bit type definitions.
  */
 #ifndef HAVE_STANDARD_INTEGER_TYPES
-typedef s8      int8_t
-typedef u8      uint8_t
-typedef s16     int16_t
-typedef u16     uint16_t
-typedef s32     int32_t
-typedef u32     uint32_t
-typedef s64     int64_t
-typedef u64     uint64_t
+typedef s8      int8_t;
+typedef u8      uint8_t;
+typedef s16     int16_t;
+typedef u16     uint16_t;
+typedef s32     int32_t;
+typedef u32     uint32_t;
+typedef s64     int64_t;
+typedef u64     uint64_t;
 #define HAVE_STANDARD_INTEGER_TYPES
 #endif
 
